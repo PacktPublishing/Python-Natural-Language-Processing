bb347863704f7845e9024134c3c9a7621efaadec
sayli2212 <sayalin@packtpub.com>
2017-07-31 12:40:17 +0530

    Add files via upload

---
+++ Chapter04/4_3_stopwordremove.py
@@ -0,0 +1,31 @@
+from nltk.corpus import stopwords
+
+
+def stopwordlist():
+    stopwordlist = stopwords.words('english')
+    print ""
+    print "------List of stop words-------"
+    for s in stopwordlist:
+        print s
+
+def customizedstopwordremove():
+    stop_words = set(["hi", "bye"])
+    line = """hi this is foo. bye"""
+    print ""
+    print "--------Customized stopword removal---------"
+    print " ".join(word for word in line.split() if word not in stop_words)
+
+def stopwordremove():
+    stop = set(stopwords.words('english'))
+    sentence = "this is a test sentence. I am very happy today."
+    print ""
+    print "--------Stop word removal from raw text---------"
+    print " ".join([i for i in sentence.lower().split() if i not in stop])
+
+
+
+
+if __name__ == "__main__":
+    stopwordlist()
+    customizedstopwordremove()
+    stopwordremove()
